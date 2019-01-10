f4bc6eae1fbccf46a8edb9928ca4d3090022305a
imanahsann <imanahsan@gmail.com>
2019-01-09 21:38:10 -0500

    cleaned JS and responsive styles

--- scripts/script.js
+++ scripts/script.js
@@ -8,37 +8,46 @@ $(function () {
 
 // run through functions
 quizApp.init = function () {
+    quizApp.smoothScroll();
     quizApp.formSubmit();
+    quizApp.tryAgain();
 }
 
 // set score and matching donuts
 quizApp.donuts = [
     {
-        donut: 'Original glazed',
-        score: 4,
-        description: `You're independent and reliable. Your friends and family can count on you. You enjoy the classics and sometimes miss the simple way things used to be. You definitely have a record player with old records lining your shelves. You have a good head on your shoulders, but sometimes you're averse to taking risks. Add some adventure to your life!`
+        donut: 'original-glazed',
+        score: 4
     },
     {
-        donut: 'Strawberry iced',
-        score: 8,
-        description: `You are creative and fun. You have the amazing ability to make friends wherever you go. You are kind and empathetic. Just remember to say no and avoid taking on more than you can handle. You have needs too!`
+        donut: 'strawberry-iced',
+        score: 8
     },
     {
-        donut: 'Chocolate iced',
-        score: 12,
-        description: `You're bold and trustworthy. Your friends can always count on you to honestly approve or veto their outfit or selfie. You're open to trying new things. Don't be afraid to get in touch with your emotions. Watch that rom-com, have that cry!`
+        donut: 'chocolate-iced',
+        score: 12
     },
     {
-        donut: 'Boston cream',
-        score: 16,
-        description: `You have so much to offer. You basically ooze charm and flavour. Your friends appreciate your ability to break the ice no matter the situation. Your sense of humour makes you an invaluable friend, but don't forget to prioritize yourself and experience your emotions to the fullest.`
+        donut: 'boston-cream',
+        score: 16
     },
     {
-        donut: 'Chocolate iced with sprinkles',
-        score: 20,
-        description: `Are you okay, babe? Just kidding, we approve of your adventurous, try-anything attitude towards life. You've always been one to ride the rollercoaster of life, never passing up an opportunity to coast straight to skyhigh euphoria. But don't forget to incorporate self-care into your routine so that the deep dives are less traumatic and more therapeutic.`
+        donut: 'chocolate-iced-with-sprinkles',
+        score: 20
     }
 ]
+
+// smooth scroll links
+quizApp.smoothScroll = function(){
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        // prevent default link behaviour
+        event.preventDefault();
+        // scroll to destination
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+};
 
 // on submit
 quizApp.formSubmit = function(){
@@ -47,18 +56,9 @@ quizApp.formSubmit = function(){
         event.preventDefault();
         // check if all questions answered
         if ( $('input:checked').length < 4 ) {
-            // remove content if any
-            $('.results').empty();
-            // post error on page if unanswered questions
-            $('.results').append('<p class="icon"><i class="fas fa-exclamation-circle"></i></p><p class="message"><a href="#start" class="button">Please answer all questions!</a></p>');
-            // show results container
-            $(".results-container").css("display", "block");
-            // hide results header
-            $(".results-container h2").css("display", "none");
-            // scroll to error message
-            document.getElementById('results').scrollIntoView();
+            // sweet alert
+            swal("Error", "Please answer all questions!", "error");
         }
-
         // retrieve checked values, convert to array, convert to numbers, add together for total points
         else {
             quizApp.points = $('input:checked').map(function () {
@@ -68,9 +68,7 @@ quizApp.formSubmit = function(){
             }).reduce(function (acc, curr) {
                 return acc + curr;
             });
-            // console.log(quizApp.points);
             quizApp.result();
-            quizApp.descriptions();
             quizApp.postResult();
         }
     })
@@ -86,29 +84,37 @@ quizApp.result = function(){
     }
 };
 
-quizApp.descriptions = function () {
-    // compare quizApp.points to donut score, return description of donut
-    for (let i = 0; i < quizApp.donuts.length; i++) {
-        if (quizApp.points <= quizApp.donuts[i].score) {
-            quizApp.description = quizApp.donuts[i].description;
-            return
-        }
-    }
+quizApp.postResult = function(){
+    // hide previous result, if any
+    $(".original-glazed").addClass("hide");
+    $(".strawberry-iced").addClass("hide");
+    $(".chocolate-iced").addClass("hide");
+    $(".boston-cream").addClass("hide");
+    $(".chocolate-iced-with-sprinkles").addClass("hide");
+    // show result
+    $(`.${quizApp.donut}`).removeClass("hide");
+    // show results container
+    $(".results-container").removeClass("hide");
+    // smooth scroll to results
+    $('html, body').animate({
+        scrollTop: $("#results").offset().top
+    }, 500);
 };
 
-quizApp.postResult = function(){
-    // remove content if any
-    $('.results').empty();
-    // post result on page
-    $('.results').append(`<p class="donut-name">${quizApp.donut} donut!</p><p class="donut description">${quizApp.description}</p><p class="icon"><a href="#end"><i class="fas fa-arrow-circle-down"></i></a></p>`);
-    // remove try again content if any
-    $('.try-again').empty();
-    // add option to try again on page
-    $('.try-again').append(`<p class="icon"><i class="fas fa-smile"></i></p><p class="try-again"><a href="#credits" class="button">Credits.</a></p><p class="icon"><i class="fas fa-sad-tear"></i></p><p class="try-again"><a href="#start" class="button">This isn't me... Try again.</a></p>`);
-    // show results container, results heading and end section
-    $(".results-container").css("display", "block");
-    $(".results-container h2").css("display", "block");
-    $(".end").css("display", "block");
-    // scroll to results
-    document.getElementById('results').scrollIntoView();
+// on try again
+quizApp.tryAgain = function () {
+    $('.try-again').on('click', function (event) {
+        // prevent default link behaviour
+        event.preventDefault();
+        // clear results
+        $(".results-container").addClass("hide");
+        // uncheck all answers
+        $('input:radio').each(function(){
+            $(this).attr('checked', false);
+        });
+        // smooth scroll to start
+        $('html, body').animate({
+            scrollTop: $("#start").offset().top
+        }, 500);
+    });
 };
